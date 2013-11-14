c5736699031ae4e18fbbca2fc47a408dbd2c473e
Egor Potemkin <evp3103@iMac-Egor.local>
2013-11-14 17:40:48 +0400

    - bin&gen folders

--- gen/com/github/evp3103/android/support/v4/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.github.evp3103.android.support.v4;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
