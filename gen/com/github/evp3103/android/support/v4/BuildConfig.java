81e712eb64f34d2a6f6e46dbc3eea61e9583444b
Egor Potemkin <evp3103@iMac-Egor.local>
2013-11-14 17:36:29 +0400

    + android support v4 r19

---
+++ gen/com/github/evp3103/android/support/v4/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.github.evp3103.android.support.v4;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
